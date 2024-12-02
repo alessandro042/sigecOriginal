27834e3a91d9b81f9df434dd6717ac58c1525dc3
Irving Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2024-12-01 20:12:38 -0600

    Se agregaron las imagenes al Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -32,27 +32,27 @@
                 <h5 style="font-size: 14px; font-weight: bold; margin-bottom: 30px;">Nuestros socios estratégicos</h5>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fab fa-coca-cola" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/coca-cola.png') }}" alt="Coca-Cola" style="width: 50px;">
                 <p style="font-size: 12px; margin-top: 5px;">Coca-Cola</p>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fas fa-cookie-bite" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/sabritas.png') }}" alt="Sabritas" style="width: 36px;">
                 <p style="font-size: 12px; margin-top: 5px;">Sabritas</p>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fas fa-bread-slice" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/bimbo.png') }}" alt="Bimbo" style="width: 60px;">
                 <p style="font-size: 12px; margin-top: 5px;">Bimbo</p>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fas fa-cheese" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/lala.png') }}" alt="Lala" style="width: 60px;">
                 <p style="font-size: 12px; margin-top: 5px;">Lala</p>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fas fa-ice-cream" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/nestle.png') }}" alt="Nestlé" style="width: 36px;">
                 <p style="font-size: 12px; margin-top: 5px;">Nestlé</p>
             </div>
             <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-3">
-                <i class="fas fa-dog" style="font-size: 36px; color: #fff;"></i>
+                <img src="{{ asset('img/purina.png') }}" alt="Purina" style="width: 40px;">
                 <p style="font-size: 12px; margin-top: 5px;">Purina</p>
             </div>
         </div>
